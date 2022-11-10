c8677b2b1f22c99c856a138e4ba97d311be7789d
Matthias Vach <matthias.vach@sap.com>
2022-11-10 09:29:05 +0100

    Add initial put implementation

--- client/client.go
+++ client/client.go
@@ -1,8 +1,11 @@
 package client
 
 import (
+	"context"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/mvach/bosh-azure-storage-cli/config"
+	"log"
+	"os"
 )
 
 type AzBlobstore struct {
@@ -12,4 +15,14 @@ type AzBlobstore struct {
 
 func New(azClient *azblob.Client, storageConfig *config.AZStorageConfig) (AzBlobstore, error) {
 	return AzBlobstore{azClient: azClient, storageConfig: storageConfig}, nil
+}
+
+func (client *AzBlobstore) Put(file *os.File, dest string) error {
+	//TODO: Check if we need to set a "WithTimeout(parent Context, timeout time.Duration)" context here:
+	response, err := client.azClient.UploadFile(context.Background(), "test2", "dev1", file, nil)
+	if err == nil {
+		log.Println(response)
+	}
+
+	return err
 }
